80b32b264bede5a62c17ce26db11b48ad63fc889
sushant0511 <92315598+sushant0511@users.noreply.github.com>
2024-05-20 15:30:47 +0530

    Spring Security

---
+++ src/main/java/com/example/SpringSecurtyTest/SpringSecurtyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurtyTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurtyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurtyTestApplication.class, args);
+	}
+
+}
